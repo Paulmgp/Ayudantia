ea2d4b0d5b8bd865e2f7ab404b7cff00fc7081ea
Paulmgp <paulmgp2002@hotmail.com>
2023-10-18 10:35:47 -0500

    Commit por Paulmgp

--- src/ayudantia/Ayudantia.java
+++ src/ayudantia/Ayudantia.java
@@ -16,6 +16,7 @@ public class Ayudantia {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola Mundo 2");
+        System.out.println("Modificado por Paulmgp");
     }
 
 }
