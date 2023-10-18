c35f56ecbdba1b2752646c56289cd7febe0363da
Paulmgp <paulmgp2002@hotmail.com>
2023-10-18 10:50:43 -0500

    Login

---
+++ src/ayudantia/Login.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ayudantia;
+
+/**
+ *
+ * @author Paul Gudiño
+ */
+public class Login {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Login");
+    }
+
+}
