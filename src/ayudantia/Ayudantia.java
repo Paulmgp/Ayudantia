491e8e532126c4e45a0a45297d1cb5b653c6d602
Paulmgp <paulmgp2002@hotmail.com>
2023-10-18 10:39:03 -0500

    Elimacion Hola Mundo

--- src/ayudantia/Ayudantia.java
+++ src/ayudantia/Ayudantia.java
@@ -15,7 +15,6 @@ public class Ayudantia {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola Mundo 2");
         System.out.println("Modificado por Paulmgp");
     }
 
