9cd0e769192436c60538d09c1104e36b77a80b10
Paul Gudiño <74059174+PaulGudino@users.noreply.github.com>
2023-10-18 10:33:28 -0500

    Commit hecho por PaulGudino

--- src/ayudantia/Ayudantia.java
+++ src/ayudantia/Ayudantia.java
@@ -15,7 +15,7 @@ public class Ayudantia {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo 2");
     }
 
 }
